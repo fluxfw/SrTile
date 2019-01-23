6d5b08692a221af75ad8bf03e306b7e8259ae758
Martin Studer <ms@studer-raimann.ch>
2019-01-23 18:55:17 +0100

    Fixes for Certificates 1. tile shows certificate of current object - not of submodules; 2. download link for certificates for course; 3. only show certificate the tile has enabled the option show certificates

--- src/Certificate/Certificates.php
+++ src/Certificate/Certificates.php
@@ -3,9 +3,12 @@
 namespace srag\Plugins\SrTile\Certificate;
 
 use ilCertificate;
+use ilCourseParticipant;
 use ilLPStatus;
 use ilObject;
+use ilObjectDataCache;
 use ilObjUser;
+use ilCourseParticipants;
 use ilSAHSPresentationGUI;
 use ilSrTilePlugin;
 use srag\DIC\SrTile\DICTrait;
@@ -79,72 +82,37 @@ class Certificates {
 	}
 
 
-	/**
-	 * @return string|null
-	 */
-	public function getCertificateBatchForUserOnCourseOrCourseSubModule()/*: ?string*/ {
-		switch (ilObject::_lookupType($this->obj_id)) {
-			case 'crs':
-				return $this->getCertificateBatchOfModuleWithActiveCertificateInCourse();
-
-			default:
-				return $this->getCertificateBatchOfModuleWithActiveCertificate();
-		}
-	}
-
 
 	/**
-	 * @return string|null
-	 */
-	protected function getCertificateBatchOfModuleWithActiveCertificateInCourse()/*: ?string*/ {
-		$query = 'SELECT cert.obj_id AS mod_obj_id, mod_ref.ref_id AS mod_ref_id FROM il_certificate AS cert
-					INNER JOIN object_reference AS mod_ref ON mod_ref.obj_id = cert.obj_id
-					INNER JOIN tree ON tree.child = mod_ref.ref_id
-					INNER JOIN object_reference AS crs_ref ON crs_ref.ref_id = tree.parent
-					INNER JOIN object_data AS crs_obj ON crs_obj.obj_id = crs_ref.obj_id
-						AND crs_obj.type=%s AND crs_obj.obj_id=%s';
-
-		$res = self::dic()->database()->queryF($query, [ "text", "integer" ], [ "crs", $this->obj_id ]);
-
-		while (($row = $res->fetchAssoc()) !== false) {
-			return $this->returnDownloadLink($row['mod_ref_id']);
-		}
-
-		return NULL;
-	}
-
-
-	/**
-	 * @return string|null
-	 */
-	protected function getCertificateBatchOfModuleWithActiveCertificate()/*: ?string*/ {
-		$query = 'SELECT cert.obj_id AS mod_obj_id, mod_ref.ref_id AS mod_ref_id FROM il_certificate AS cert
-					INNER JOIN object_reference AS mod_ref ON mod_ref.obj_id = cert.obj_id
-					WHERE cert.obj_id=%s';
-
-		$res = self::dic()->database()->queryF($query, [ "integer" ], [ $this->obj_id ]);
-
-		while (($row = $res->fetchAssoc()) !== false) {
-			return $this->returnDownloadLink($row['mod_ref_id']);
-		}
-
-		return NULL;
-	}
-
-
-	/**
+	 * @param string $obj_type
 	 * @param int $obj_ref_id
 	 *
 	 * @return string|null
 	 */
-	protected function returnDownloadLink(int $obj_ref_id)/*: ?string*/ {
-		if ($obj_ref_id > 0 && self::ilias()->learningProgress($this->user)->getStatus($obj_ref_id) == ilLPStatus::LP_STATUS_COMPLETED_NUM) {
-			return 'ilias.php?baseClass=' . ilSAHSPresentationGUI::class . '&ref_id=' . $obj_ref_id . '&cmd=downloadCertificate';
+	public function getCertificateDownloadLink()/*: ?string*/ {
 
-			//the following way of link generation does not work! the above way is the standard(!:-( ILIAS way of link generation for certificate
-			//$this->ctrl->setParameterByClass(ilSAHSPresentationGUI::class, 'ref_id', $obj_ref_id);
-			//return $this->ctrl->getLinkTargetByClass(ilSAHSPresentationGUI::class,'downloadCertificate');
-		} else {
+		//@see Modules/Course/classes/class.ilObjCourseGUI.php:3214
+		if (ilCertificate::isActive() &&
+			ilCertificate::isObjectActive($this->obj_id)) {
+
+			//TODO Certificates for ILIAS Test
+			switch (self::dic()->objDataCache()->lookupType($this->obj_id)) {
+				case "crs":
+					//@see Modules/Course/classes/class.ilObjCourseGUI.php:3214
+					if (ilCourseParticipants::getDateTimeOfPassed($this->obj_id, self::dic()->user()->getId())) {
+						self::dic()->ctrl()->setParameterByClass("ilObjCourseGUI", "ref_id", $this->obj_ref_id);
+
+						return self::dic()->ctrl()->getLinkTargetByClass([ "ilRepositoryGUI", "ilObjCourseGUI" ], 'deliverCertificate');
+					}
+				case "sahs":
+					if (self::ilias()->learningProgress($this->user)->getStatus($this->obj_ref_id) == ilLPStatus::LP_STATUS_COMPLETED_NUM) {
+						//the following way of link generation does not work! the above way is the standard(!:-( ILIAS way of link generation for certificate
+						//$this->ctrl->setParameterByClass(ilSAHSPresentationGUI::class, 'ref_id', $obj_ref_id);
+						//return $this->ctrl->getLinkTargetByClass(ilSAHSPresentationGUI::class,'downloadCertificate');
+						return 'ilias.php?baseClass=' . ilSAHSPresentationGUI::class . '&ref_id=' . $this->obj_ref_id . '&cmd=downloadCertificate';
+					}
+			}
+
 			return NULL;
 		}
 	}
