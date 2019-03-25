4ded1bf1e1ba9e3a5122f69e77cf25de1cdeb37e
Florian Wyss <fw@studer-raimann.ch>
2019-03-25 10:41:59 +0100

    Fix possible config crash

--- classes/class.ilSrTileConfigGUI.php
+++ classes/class.ilSrTileConfigGUI.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . "/../vendor/autoload.php";
+
 use srag\ActiveRecordConfig\SrTile\ActiveRecordConfigGUI;
 use srag\Plugins\SrTile\Config\ConfigFormGUI;
 use srag\Plugins\SrTile\Template\Template;
