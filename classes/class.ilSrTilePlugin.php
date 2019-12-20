b07bdc55d2b69cac1c28af850e296c1c5e237e30
Florian Wyss <fw@studer-raimann.ch>
2019-12-20 10:13:33 +0100

    Update srag libraries

--- classes/class.ilSrTilePlugin.php
+++ classes/class.ilSrTilePlugin.php
@@ -22,7 +22,6 @@ class ilSrTilePlugin extends ilUserInterfaceHookPlugin
     const PLUGIN_ID = "srtile";
     const PLUGIN_NAME = "SrTile";
     const PLUGIN_CLASS_NAME = self::class;
-    const REMOVE_PLUGIN_DATA_CONFIRM_CLASS_NAME = SrTileRemoveDataConfirm::class;
     const WEB_DATA_FOLDER = self::PLUGIN_ID . "_data";
     const EVENT_CHANGE_TILE_BEFORE_RENDER = "change_title_before_render";
     const EVENT_SHOULD_NOT_DISPLAY_ALERT_MESSAGE = "should_not_display_alert_message";
