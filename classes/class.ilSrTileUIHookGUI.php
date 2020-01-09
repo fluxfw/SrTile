298c901f4d247888133aabfc72d34c690ef1db0c
Florian Wyss <fw@studer-raimann.ch>
2020-01-09 09:58:53 +0100

    Fix

--- classes/class.ilSrTileUIHookGUI.php
+++ classes/class.ilSrTileUIHookGUI.php
@@ -1,6 +1,5 @@
 <?php
 
-use ILIAS\UI\Component\Component;
 use srag\DIC\SrTile\DICTrait;
 use srag\Plugins\SrTile\Config\Config;
 use srag\Plugins\SrTile\Recommend\RecommendGUI;
@@ -128,23 +127,20 @@ class ilSrTileUIHookGUI extends ilUIHookPluginGUI
                     if (self::srTile()->access()->hasWriteAccess($obj_ref_id)) {
                         self::dic()->ctrl()->setParameterByClass(TileGUI::class, TileGUI::GET_PARAM_REF_ID, $obj_ref_id);
 
-                        $actions_html = self::output()->getHTML(array_map(function (Component $action) : string {
-                            return '<li>' . self::output()->getHTML($action) . '</li>';
-                        }, [
-                            self::dic()->ui()->factory()->link()->standard('<span class="xsmall">' . self::plugin()->translate("edit_tile", TileGUI::LANG_MODULE) . '</span>',
+                        $edit_tile_html = '<li>' . self::output()->getHTML(self::dic()->ui()->factory()->link()->standard('<span class="xsmall">' . self::plugin()
+                                    ->translate("edit_tile", TileGUI::LANG_MODULE) . '</span>',
                                 self::dic()->ctrl()->getLinkTargetByClass([
                                     ilUIPluginRouterGUI::class,
                                     TileGUI::class
-                                ], TileGUI::CMD_EDIT_TILE))
-                        ]));
+                                ], TileGUI::CMD_EDIT_TILE))) . '</li>';
 
                         $matches = [];
                         preg_match('/<ul class="dropdown-menu pull-right" role="menu" id="ilAdvSelListTable_.*">/',
                             $html, $matches);
                         if (is_array($matches) && count($matches) >= 1) {
-                            $html = str_ireplace($matches[0], $matches[0] . $actions_html, $html);
+                            $html = str_ireplace($matches[0], $matches[0] . $edit_tile_html, $html);
                         } else {
-                            $html = $actions_html . $html;
+                            $html = $edit_tile_html . $html;
                         }
 
                         return ["mode" => self::REPLACE, "html" => $html];
