e9e01c6d1d9a4f30b0fe43efd7df147b536a6ce4
fluxfw <fluxfw@fluxlabs.ch>
2022-03-17 11:16:00 +0100

    Move `ilias7_core_apply_ilctrl_patch.sh`

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = "srtile";
-$version = "1.12.3";
+$version = "1.12.4";
 $ilias_min_version = "6.0";
 $ilias_max_version = "7.999";
 $responsible = "fluxlabs";
